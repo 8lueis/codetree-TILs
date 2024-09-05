92a13cc4a29e8bbe6f6535513d64d7044d4b8e14
8lue <115973845+8lueis@users.noreply.github.com>
2024-09-05 11:15:03 +0900

    Title: 합을 복사하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240905/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1, b = 2, c = 3;
+        int r = a+b+c;
+        a = b = c = r;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
